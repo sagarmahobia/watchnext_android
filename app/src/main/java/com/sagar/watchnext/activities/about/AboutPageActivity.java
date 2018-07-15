9597eb2730e9f6f4c711f2cfc3979dd2329387f4
Sagar Mahobia <sagarmahobia5@gmail.com>
2018-07-15 23:30:35 +0530

    about page working. removed powered by tmdb logo and created share button.

--- app/src/main/java/com/sagar/watchnext/activities/about/AboutPageActivity.java
+++ app/src/main/java/com/sagar/watchnext/activities/about/AboutPageActivity.java
@@ -1,15 +1,93 @@
 package com.sagar.watchnext.activities.about;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.sagar.watchnext.R;
+import com.sagar.watchnext.WatchNextApplication;
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class AboutPageActivity extends AppCompatActivity {
+
+    @BindView(R.id.about_logo_image)
+    ImageView logoImage;
+
+    @BindView(R.id.tmdb_image_license_text)
+    TextView tmdbLicense;
+
+    @BindView(R.id.tmdb_disclaimer_text)
+    TextView tmdbDisclaimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_page);
+
+        ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        ButterKnife.bind(this);
+
+        Picasso picasso = WatchNextApplication.get(this).getComponent().providesPicasso();
+        picasso.load(R.drawable.watch_next_feature_graphics).into(logoImage);
+
+
+        String imageLicenseText = "WatchNext uses data and images by TMDb licensed under ";
+        String imageLinkText = "CC BY-NC 4.0";
+
+        SpannableString tmdbLicenseSpan = new SpannableString(imageLicenseText + imageLinkText);
+        tmdbLicenseSpan.setSpan(new ClickableSpan() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("https://creativecommons.org/licenses/by-nc/4.0/"));
+                startActivity(i);
+            }
+        }, imageLicenseText.length(), imageLicenseText.length() + imageLinkText.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+
+        tmdbLicense.setText(tmdbLicenseSpan);
+        tmdbLicense.setMovementMethod(LinkMovementMethod.getInstance());
+
+        String disclaimerText = "WatchNext uses the TMDb API but is not endorsed or certified by TMDb: - ";
+        String disclaimerLink = "https://www.themoviedb.org/documentation/api/terms-of-use";
+
+        SpannableString tmdbDisclaimerSpan = new SpannableString(disclaimerText + disclaimerLink);
+
+        tmdbDisclaimerSpan.setSpan(new ClickableSpan() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(disclaimerLink));
+                startActivity(i);
+            }
+        }, disclaimerText.length(), disclaimerText.length() + disclaimerLink.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+        tmdbDisclaimer.setText(tmdbDisclaimerSpan);
+        tmdbDisclaimer.setMovementMethod(LinkMovementMethod.getInstance());
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (android.R.id.home == id) {
+            this.onBackPressed();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/sagar/watchnext/activities/main/MainActivity.java
+++ app/src/main/java/com/sagar/watchnext/activities/main/MainActivity.java
@@ -17,6 +17,7 @@ import android.widget.Toast;
 
 import com.sagar.watchnext.R;
 import com.sagar.watchnext.WatchNextApplication;
+import com.sagar.watchnext.activities.about.AboutPageActivity;
 import com.sagar.watchnext.activities.main.home.HomeFragment;
 import com.sagar.watchnext.activities.main.movies.MoviesFragment;
 import com.sagar.watchnext.activities.main.tv.TvFragment;
@@ -83,7 +84,8 @@ public class MainActivity extends AppCompatActivity
             Intent intent = new Intent(this, SearchActivity.class);
             startActivity(intent);
         } else if (R.id.action_about == id) {
-            Toast.makeText(this, "Feature N/A", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, AboutPageActivity.class);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
@@ -104,6 +106,13 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.tv) {
             fragment = TvFragment.newInstance();
 
+        } else if (id == R.id.nav_share) {
+            String shareBody = "WatchNext - Discover Movies & TV. \n" +
+                    "https://play.google.com/store/apps/details?id=com.sagar.watchnext";
+            Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+            sharingIntent.setType("text/plain");
+            sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+            this.startActivity(Intent.createChooser(sharingIntent, "Share via"));
         }
         if (fragment != null) {
             transactFragment(fragment);
